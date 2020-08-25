c48348dccd800c2c07546685ee5a291151197d7c
Sam Debruyn <sam@debruyn.dev>
2020-08-25 12:10:55 +0200

    add logs

--- test/module_test.go
+++ test/module_test.go
@@ -38,11 +38,14 @@ func TestRotationEvery3MinutesWith2MinuteMargin(t *testing.T) {
 
 	ctx := context.Background()
 
+	t.Logf("Creating storage account %s & container %s", accountName, name)
 	container, blobEndpoint, err := setupStorageContainer(ctx, name, accountName, groupName, region, subscriptionID, clientID, clientSecret, tenantID)
 	assert.NoError(t, err)
 	assert.NotNil(t, container)
+	t.Logf("Created storage account %s and container %s", accountName, name)
 
 	defer func() {
+		t.Log("Destroying resource group")
 		_, err = destroyResourceGroup(ctx, groupName, subscriptionID, clientID, clientSecret, tenantID)
 		assert.NoError(t, err)
 	}()
@@ -62,33 +65,49 @@ func TestRotationEvery3MinutesWith2MinuteMargin(t *testing.T) {
 	terraformOptions.Vars["storage_account_name"] = accountName
 
 	defer terraform.Destroy(t, terraformOptions)
+	t.Log("Running Terraform apply")
 	_, err = terraform.InitAndApplyE(t, terraformOptions)
 	assert.NoError(t, err)
 
+	t.Log("Getting first SAS token")
 	sasToken, err := terraform.OutputE(t, terraformOptions, "sas")
 	assert.NoError(t, err)
 
 	assert.NotEmpty(t, sasToken)
 
+	t.Log("Trying blob upload with first SAS token, should succeed")
 	err = tryBlobUpload(name, *blobEndpoint, sasToken, ctx)
 	assert.NoError(t, err)
 
+	t.Log("Running Terraform apply again")
 	_, err = terraform.ApplyE(t, terraformOptions)
-	secondSasToken, err := terraform.OutputE(t, terraformOptions, "sas")
+	assert.NoError(t, err)
 
+	t.Log("Getting second SAS token, should not be different from first")
+	secondSasToken, err := terraform.OutputE(t, terraformOptions, "sas")
+	assert.NoError(t, err)
 	assert.Equal(t, sasToken, secondSasToken)
+
+	t.Log("Sleep for 3 minutes")
 	time.Sleep(3 * time.Minute)
 
+	t.Log("Trying blob upload with first SAS token, should still work")
 	err = tryBlobUpload(name+"2", *blobEndpoint, sasToken, ctx)
 	assert.NoError(t, err)
 
+	t.Log("Running Terraform apply again")
 	_, err = terraform.ApplyE(t, terraformOptions)
+	assert.NoError(t, err)
+
+	t.Log("Getting third SAS token, should be different from first")
 	thirdSasToken, err := terraform.OutputE(t, terraformOptions, "sas")
 	assert.NotEqual(t, sasToken, thirdSasToken)
 
+	t.Log("Trying blob upload with third SAS token, should succeed")
 	err = tryBlobUpload(name+"3", *blobEndpoint, thirdSasToken, ctx)
 	assert.NoError(t, err)
 
+	t.Log("Trying blob upload with first SAS token, should not work anymore")
 	err = tryBlobUpload(name+"4", *blobEndpoint, sasToken, ctx)
 	assert.Error(t, err)
 }
