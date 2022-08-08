b6dc03cf32224a3dc1bcf60bf499a166de955ccb
Freia Vercruysse <freia.vercruysse@lynxcare.eu>
2022-08-08 16:59:49 +0200

    add expiry date sas as output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "sas" {
   value       = var.storage_container_name == null || var.storage_container_name == "" ? data.azurerm_storage_account_sas.sas[0].sas : data.azurerm_storage_account_blob_container_sas.sas[0].sas
   sensitive   = true
 }
+
+output "expiry_date" {
+  description = "expiry date"
+  value       = local.expiration
+}
